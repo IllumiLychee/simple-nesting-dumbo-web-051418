0ce95ed17f07070480471829ea368b46a2163c46
Angelina Wong <angelina.w25@gmail.com>
2018-05-09 17:38:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+	programmer_hash[:grace_hopper]	 
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +56,15 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+	programmer_hash[:dennis_ritchies][:languages]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+#:yukihiro_matsumoto => {
+   #:known_for => "Ruby",
+   #:languages => ["LISP", "C"]
+ #
 
 	programmer_hash = 
  		{
@@ -80,7 +82,8 @@ def adding_matz
         }
      }
 
-    
+	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash
 end
 
 def changing_alan
