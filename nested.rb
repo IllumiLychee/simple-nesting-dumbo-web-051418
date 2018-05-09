6e530d6abb3249d01fb76187eeb1a96416806ef3
Angelina Wong <angelina.w25@gmail.com>
2018-05-09 17:43:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-require pry
+
 def hopper
 	programmer_hash =
  		{
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-	programmer_hash[:alan_kay][:known_for]binding.pry
+	programmer_hash[:alan_kay][:known_for] binding.pry
 end
 
 def dennis_ritchies_language
